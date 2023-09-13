29e22b4482a87be812c8eb368f9a5790eff9e1ee
RogerioSobral <sobralcomix@gmail.com>
2023-09-13 11:03:17 -0300

    resolução de trabalho exe5

--- exe5.py
+++ exe5.py
@@ -1,7 +1,21 @@
+# -*- coding: latin-1 -*-
+
 # Você deve criar uma aplicação que vai cadastrar 4 topicos
 #salvando esses topicos, na seguinte formatação dentro do arquivo txt
-
+import sys
 #|__Topico 1
 #    |__Topico 2
 #        |__Topico 3
 #            |__Topico 4
+
+try:
+    arquivo=open("topicos.txt","+a")
+    tab = ""
+    for i in range(4):
+        topico = input("Digite o topico Desejado")
+            # "|__Topico 1
+            #    |__Topico 2
+        arquivo.write(f'{tab}|__{topico}\n')
+        tab = tab+"\t"
+except Exception as e:
+    print("Não foi possivel salvar dentro do arquivo\nErro:",e)

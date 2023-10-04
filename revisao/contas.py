8e99b1407f40f15340a37a3cf17f81aa638b576d
RogerioSobral <sobralcomix@gmail.com>
2023-10-04 12:09:21 -0300

    Herança, Classes Abstratas

---
+++ revisao/contas.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+
+class Conta(ABC):
+
+    def __init__(self, valor, cliente):
+        self.__saldo=valor
+        self.__cliente=cliente
+        self.__status=True
+
+
+    @property
+    def status(self):
+        return self.__status
+
+    def alterarStatus(self, status:bool):
+        self.__status=status
+
+
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo=valor
+
+ #Quando eu usar esse metodo tenho que tratar o erro
+    def deposito(self, valor):
+        if self.status:
+            self.__saldo += valor
+        else:
+            raise "A conta não esta ativa"
+
+    @abstractmethod
+    def saque(self,valor): # assinatura
+        pass
+
+
+class ContaCorrente(Conta):
+
+    def __init__(self,valor:float, cliente,limite:float):
+        super().__init__(valor, cliente)
+        self.__limite=limite
+
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    def saque(self,valor):
+        if self.status:
+            if isinstance(valor,(int, float)):
+                if self.saldo>= valor:
+                    self.saldo = valor
+                elif self.saldo+self.__limite>=valor:
+                    self.saldo=self.saldo-valor
+                    self.__limite=self.__limite+self.saldo
+                    print("Você esta no limite")
+                else:
+                    raise "Saldo não cobre o valor de saque"
+            else:
+                raise "Digite um valor valido"
+        else:
+            raise "A conta não esta ativa"
+
+
+
+class ContaPoupanca(Conta):
+
+    def saque(self,valor):
+        if self.status:
+            if self.saldo>=valor:
+                self.saldo-=valor
+            else:
+                raise "Você não tem saldo."
+        else:
+            raise "A conta não esta ativa"
+
+
+
+
+if __name__ == '__main__':
+    cp= ContaPoupanca(1200,"Carlos")
+    cc = ContaCorrente(1100, "Maria",1000)
+
+    print(cp.saldo)
+    cp.deposito(350)
+    print(cp.saldo)
+    cp.saque(1230.56)
+    print(cp.saldo)
+
+    cp.saque(300.56)
+    print(cp.saldo)
+
+
+
+    print("#" * 30)
+    print(cc.saldo)
+    cc.deposito(350)
+    print(cc.saldo)
+    cc.saque(1700)
+    print(cc.saldo)
+    print("Limite")
+    print(cc.limite)
--- revisao/revisao1.py
+++ revisao/revisao1.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import PySimpleGUI as sg
 #Tela
+# CONTROLE DE CONTA
+
 
 fonte_texto=("roboto",12)
 fonte_titulo=("roboto",14,"bold")
@@ -8,7 +10,9 @@ fonte_titulo=("roboto",14,"bold")
 def janelaMenu():
     layout=[
         [sg.Push(background_color="white"), sg.Text("Menu",background_color="white", font=fonte_titulo),sg.Push(background_color="white")],
-        [sg.Button(key="-MENU_CADASTRAR-",button_text= "Cadastrar",font=fonte_titulo)]
+        [sg.Button(key="-MENU_CADASTRAR-",button_text= "Cadastrar Cliente",font=fonte_titulo)],
+        [sg.Button(key="-DEPOSITO-", button_text="Deposito", font=fonte_titulo)],
+        [sg.Button(key="-DEPOSITO-", button_text="Saque", font=fonte_titulo)],
     ]
 
     return sg.Window(title="Menu",layout=layout,finalize=True)
@@ -16,7 +20,7 @@ def janelaMenu():
 
 
 
-def janelaCadastrar():
+def janelaCadastrarCliente():
     layout=[
         [sg.Push(background_color="white"), sg.Text("Cadastro",background_color="white", font=fonte_titulo),
          sg.Push(background_color="white")],
@@ -26,12 +30,17 @@ def janelaCadastrar():
         [sg.Text("Data Nascimento", font=fonte_texto, size=10),
          sg.Image("/img/calendar.gif",background_color="white"),
          sg.Input(font=fonte_texto, size=10)],
-
-        [sg.Text("Salario",font=fonte_texto, size=10), sg.Input(key="-SALARIO-",font=fonte_texto, size=10)],
         [sg.Button(key="-MENU_CADASTRAR-",button_text="Cadastrar",font=fonte_titulo)]
     ]
 
     return sg.Window(title="Cadastro",layout=layout,finalize=True)
+
+
+
+
+
+
+
 
 
 telaMenu,telaCadastro = janelaMenu(),None
